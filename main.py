77f0f29cd22e2c546addc86b74d1b5435fa184a4
alsh12 <asheth369@gmail.com>
2022-07-13 17:36:03 +0530

    Query parameters

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import imp
+from tokenize import String
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data':{'name':'Alok'}}
+    return {'data':'blog list'}
 
-@app.get('about')
-def about():
-    return {'data':'about page'}
+
+@app.get('/blog')
+def blog(limit:int=10,published:bool=True,sort:Optional[str]=None):
+    if published:
+        return {'data':f'list of {limit} published blogs'}
+    else:
+        return {'data':f'list of {limit} blogs'}
+
+@app.get('/blog/unlisted')
+def unlisted():
+    return {'data':'unlisted blogs'}
+
+@app.get('/blog/{id}')
+def index(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def blogComents(id:int):
+    return {'data':'comments for blog '+str(id)}
